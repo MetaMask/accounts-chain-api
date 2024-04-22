ce2c1ecfacb79c9a57d6ad51af19d5c2b585a824
Charly Chevalier <charly.chevalier@consensys.net>
2024-04-22 17:26:49 +0200

    feat(caip): add support for asset id in CAIP asset type/id

--- src/caip-types.ts
+++ src/caip-types.ts
@@ -9,19 +9,45 @@ import {
   CaipReferenceStruct,
 } from '@metamask/utils';
 import type { Infer } from 'superstruct';
-import { string, pattern } from 'superstruct';
+import { is, string, pattern } from 'superstruct';
 
-// TODO: Move this to @metamask/utils
-export const CAIP_ASSET_NAMESPACE_REGEX = /^[-a-z0-9]{3,8}$/u;
-export const CAIP_ASSET_REFERENCE_REGEX = /^[-.%a-zA-Z0-9]{1,128}$/u;
 export const CAIP_ASSET_TYPE_REGEX =
   /^(?<chainId>(?<namespace>[-a-z0-9]{3,8}):(?<reference>[-_a-zA-Z0-9]{1,32}))\/(?<assetNamespace>[-a-z0-9]{3,8}):(?<assetReference>[-.%a-zA-Z0-9]{1,128})$/u;
+
+export const CAIP_ASSET_ID_REGEX =
+  /^(?<chainId>(?<namespace>[-a-z0-9]{3,8}):(?<reference>[-_a-zA-Z0-9]{1,32}))\/(?<assetNamespace>[-a-z0-9]{3,8}):(?<assetReference>[-.%a-zA-Z0-9]{1,128})\/(?<tokenId>[-.%a-zA-Z0-9]{1,78})$/u;
 
 /**
  * A CAIP-19 asset type identifier, i.e., a human-readable type of asset identifier.
  */
 export const CaipAssetTypeStruct = pattern(string(), CAIP_ASSET_TYPE_REGEX);
 export type CaipAssetType = Infer<typeof CaipAssetTypeStruct>;
+
+/**
+ * A CAIP-19 asset ID identifier, i.e., a human-readable type of asset ID.
+ */
+export const CaipAssetIdStruct = pattern(string(), CAIP_ASSET_ID_REGEX);
+export type CaipAssetId = Infer<typeof CaipAssetIdStruct>;
+
+/**
+ * Check if the given value is a {@link CaipAssetType}.
+ *
+ * @param value - The value to check.
+ * @returns Whether the value is a {@link CaipAssetType}.
+ */
+export function isCaipAssetType(value: unknown): value is CaipAssetType {
+  return is(value, CaipAssetTypeStruct);
+}
+
+/**
+ * Check if the given value is a {@link CaipAssetId}.
+ *
+ * @param value - The value to check.
+ * @returns Whether the value is a {@link CaipAssetId}.
+ */
+export function isCaipAssetId(value: unknown): value is CaipAssetId {
+  return is(value, CaipAssetIdStruct);
+}
 
 export { CaipChainIdStruct, CaipNamespaceStruct, CaipReferenceStruct };
 
